e13b83748583bbbde5a9b7c472ffdf4b994f4ee0
ojmaster <jafferyo.123@gmail.com>
2022-12-06 19:23:38 -0600

    maybe this?

--- buttons.js
+++
@@ -1,7 +0,0 @@
-function login(){
-    window.location.href = "https://ashley2098.github.io/dashboard";
-}
-
-function cancel(){
-    window.location.href = "https://ashley2098.github.io/login/Homepage";
-}
